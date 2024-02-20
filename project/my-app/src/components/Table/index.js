ec680f38568bd6ee79c2fc9a68554dd26aa509ff
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-02-20 13:52:00 +0530

    Update index.js

--- project/my-app/src/components/Table/index.js
+++ project/my-app/src/components/Table/index.js
@@ -1,10 +1,25 @@
-const TruncatedTableCell = (props) => {
-    const { text, maxLength } = props;
-    const truncatedText =
-      text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
-  
-    return <td>{truncatedText}</td>;
-  };
-  
-  export default TruncatedTableCell;
-  
+import {useEffect} from 'react'
+
+import './index.css'
+
+const TruncatedTableCell = props => {
+  const {text, maxLength, image} = props
+
+  useEffect(() => {
+    console.log(text)
+  }, [text])
+
+  const truncatedText =
+    text.length > maxLength ? `${text.slice(0, maxLength)}...` : text
+  const isImage = image === 'true'
+
+  const content = isImage ? (
+    <img src={text} alt="img" className="logo" />
+  ) : (
+    <td>{truncatedText}</td>
+  )
+
+  return <>{content}</>
+}
+
+export default TruncatedTableCell
